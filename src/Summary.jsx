31103735a3d53ef1da78deaeefb00c05f5cfa08a
joocod <20634hdj@gmail.com>
2023-12-24 21:40:07 +0900

    commit 12/24

---
+++ src/Summary.jsx
@@ -0,0 +1,7 @@
+/*
+    - 프로젝트 작업 순서
+
+    1. GlobalStyle.jsx 생성
+    2. Main.jsx 생성
+    3. Header.jsx 생성
+*/
---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RiNetflixFill } from "react-icons/ri";
+
+function Header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
