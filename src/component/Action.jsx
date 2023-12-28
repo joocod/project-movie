82811aa9c74a333a3c718cf46025a8b2db4bdce3
joocod <20634hdj@gmail.com>
2023-12-28 19:09:25 +0900

    commit 12/28

--- src/component/Action.jsx
+++ src/component/Action.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
+import {Swiper, SwiperSlide} from 'swiper/react'; 
+import { Navigation, Pagination } from 'swiper/modules';
 
 function Action() {
+
     return (
         <div>
             <Moviecontainer>
                 <Movietitle>액션</Movietitle>
+                <Swiper
+                    spaceBetween={10}
+                    slidesPerView={5}
+                    slidesPerGroup={5}
+                    loop
+                    modules={{Navigation, Pagination}}
+                    navigation
+                    pagination
+                >
+                    <Moviewrapper>
+                        {actionData.results && actionData.results.map((el,index)=>(
+                            <SwiperSlide>
+
+                            </SwiperSlide>
+                        ))}
+                    </Moviewrapper>
+                </Swiper>
             </Moviecontainer>
         </div>
     )
 }
 
 export default Action
+
+const Moviecontainer = styled.div`
+    margin-bottom: 50px;
+    position: relative;
+    box-sizing: border-box;
+`
+const Movietitle = styled.div`
+    font-size: 40px;
+    font-weight: bold;
+    color: #ffffff;
+`
+const Moviewrapper = styled.div`
+    height: 200px;
+`
